786b26732f6eea48992655854c9048665235a1d5
cetoli <cetoli@654e2ed1-901c-0410-bc1b-db0c8d10954b>
2006-10-09 04:09:58 +0000

    crude only but fails add candidat:was GET not POST?

--- tgvote/src/tgvote/controllers.py
+++ tgvote/src/tgvote/controllers.py
@@ -4,14 +4,47 @@ import cherrypy
 
 import turbogears
 from turbogears import controllers, expose, validate, redirect
+from turbogears.widgets import Widget, CSSLink, static
 
 from tgvote import json
 from turbogears import widgets, validators,url
 from urllib import urlencode
 from elementtree import ElementTree as ET
+from turbogears.widgets.datagrid import DataGrid as DG
 pleito = []
 cidadao = []
-candidato = []
+candidato = {}
+menu = [{'Eleicao':'index'
+                ,'Pleitos':'mostra_pleitos'
+                ,'Eleitores':'mostra_eleitores'
+                }]
+
+def add_custom_stdvars(vars):
+  omenu = lambda : [(menu[0][key],key) for key in menu[0].keys()]
+  return vars.update({"main_menu": omenu})
+
+turbogears.view.variable_providers.append(add_custom_stdvars)
+
+
+class MostraListagem(widgets.DataGrid):
+#  css=[widgets.CSSLink(static, "grid.css")]
+#  LocalMenu=widgets.Label(default="Vazio")
+#  Displayer=widgets.Label(default="")
+#  Displayer1=widgets.Label(default="")
+  def renderit(self,*args,**kwargs):
+    return [item() for item in self.list]
+  def display(self, value=None, **params):
+    return super(MostraListagem,self).display(value,**params),self.list[0].display(value,**params)
+  def monta(self,title,acao,conteudo):
+    self.list=[widgets.Label(default=""),widgets.Label(default="")]
+    x= widgets.TableForm("TableForm",fields=[],action=acao, submit_text=acao)
+    self.list[0] = x
+    if conteudo: 
+      self.list[1] = self #Dados().monta(conteudo)
+      self.list[0] = x
+    if not conteudo: conteudo = [{}]
+    return dict(form= self #Dados().monta(conteudo)#self #.renderit
+                ,use_case=title)
 
 class CadastroDePleito(widgets.WidgetsList):
     descricao = widgets.TextField(validator=validators.NotEmpty(),attrs={'size':80})
@@ -30,7 +63,7 @@ class CadastroDeCandidatura(widgets.WidgetsList):
                    +[(indice, candidato["Eleitor_"]) for indice,candidato in enumerate(cidadao)],
                                    default=0)
       return widgets.TableForm(fields=self,
-                                 action="http://localhost:8080/adiciona_candidato")
+                                 action="/salva_candidato")
 class CadastroDeEleitor(widgets.WidgetsList):
     nome = widgets.TextField(validator=validators.NotEmpty(),attrs={'size':80})
     senha = widgets.TextField(validator=validators.NotEmpty(),attrs={'size':20})
@@ -43,9 +76,14 @@ cadastra_pleito_form = widgets.TableForm(fields=CadastroDePleito(),
 
 class Dados:
   def monta(self,dados):
+    legenda = dados and dados[0] or {}
     return widgets.DataGrid(
-        fields=self.formalinha(dados[0]),
+        fields=self.formalinha(legenda),
         default= dados)
+        #default=[linha.values() for linha in dados])
+  def fields(self,dados):
+    legenda = dados and dados[0] or {}
+    return self.formalinha(legenda)
         #default=[linha.values() for linha in dados])
   def formalinha(self,linha):
     return [(label.strip('_'), self.formacoluna(label)) for label in linha.keys()]
@@ -56,7 +94,7 @@ class Dados:
     valor = entidade[chave]
     if chave.endswith('_'):
       #href = "<a href='/edita_%s?%s'>%s</a>"
-      href = "/edita_%s?chave=%s"
+      href = "/mostra_%s?chave=%s"
       #href = href%(chave[:-1],urlencode({'chave':valor}))
       href = href%(chave[:-1],valor)
       return self.makeLink(url=href,title=valor)
@@ -74,12 +112,34 @@ log = logging.getLogger("tgvote.controllers")
 class Root(controllers.RootController):
     @expose(template="tgvote.templates.main")
     def index(self):
-        log.debug("Happy TurboGears Controller Responding For Duty")
+      titulo="Tela Principal"
+      log.debug("Tela Principal")
+      acao='adiciona_pleito'
+      pl= pleito or [{}]
+      campos=Dados().fields(pl)
+      return MostraListagem(fields=campos,default=pl).monta(titulo,acao,pl)
+    @expose(template="tgvote.templates.main")
+    def mostra_eleitores(self):
+      titulo="Listagem dos Eleitores"
+      log.debug(titulo)
+      acao='adiciona_eleitor'
+      rol= cidadao or [{}]
+      campos=Dados().fields(rol)
+      return MostraListagem(fields=campos,default=rol).monta(titulo,acao,rol)
+    @expose(template="tgvote.templates.main")
+    def adiciona_pleito(self):
+        log.debug("Adiciona Pleito")
         return dict(
                     form=cadastra_pleito_form
-                    ,next_text='Cadastra Eleitor'
-                    ,next_url='cadastra_eleitor'
                     ,use_case='Cadastro de Pleito')
+    @expose(template="tgvote.templates.main")
+    def mostra_Pleito(self,chave): #*args,**kwargs):
+      log.debug("Cadastro de Candidaturas - chave: "+chave)
+      titulo='Cadastro de Candidaturas'
+      acao='adiciona_candidato?chave='+chave
+      rol= candidato and candidato[int(chave)] or [{}]
+      campos=Dados().fields(rol)
+      return MostraListagem(fields=campos,default=rol).monta(titulo,acao,rol)
     @expose(template="tgvote.templates.main")
     def edita_Pleito(self,chave): #*args,**kwargs):
         log.debug("chave: "+chave)
@@ -90,28 +150,50 @@ class Root(controllers.RootController):
                     ,next_url='executa_pleito'
                     ,use_case='Cadastro de Candidaturas')
     @expose(template="tgvote.templates.main")
-    def adiciona_candidato(self,Candidato,Campanha,Pleito):
+    def main_menu(self):
+      menu = [{'Eleicao_':'mostra_eleicao'
+                ,'Pleitos_':'mostra_pleitos'
+                ,'Eleitores_':'mostra_eleitores'
+                }]
+      return dict(
+                    form=Dados().monta(menu)
+                    ,next_text='Cadastra Pleito'
+                    ,next_url='index'
+                    ,use_case='Listagem dos Pleitos')
+    @expose(template="tgvote.templates.main")
+    def adiciona_candidato(self,chave):
       tb = candidato
-      pleito = int(Pleito)
-      tb[pleito] += [{'Candidato':Candidato,'Campanha':Campanha}]
+      pleito = int(chave)
+#      candidatosAoPleito= tb and tb.get(pleito) or [tb.__setitem__(pleito,[])] and tb[pleito]
+      return dict(
+                    form=CadastroDeCandidatura().monta(chave)
+                    ,next_text='Cadastra Pleito'
+                    ,next_url='index'
+                    ,use_case='Cadastro De Candidatura ao Pleito'+chave)
+    @expose(template="tgvote.templates.main")
+    def salva_candidato(self,Candidato,Campanha,Pleito):
+      tb = candidato
+      pleito = int(chave)
+      candidatosAoPleito= tb and tb.get(pleito) or [tb.__setitem__(pleito,[])] and tb[pleito]
+      candidatosAoPleito += [{'Candidato':Candidato,'Campanha':Campanha}]
       log.debug("Candidato: %s,Campanha: %s,Pleito: %s"%(Candidato,Campanha,Pleito))
       return dict(
                     form=Dados().monta(tb)
                     ,next_text='Cadastra Pleito'
                     ,next_url='index'
-                    ,use_case='Listagem dos Pleitos')
+                    ,use_case='Listagem dos Candidatos ao Pleito'+chave)
     @expose(template="tgvote.templates.main")
     def salva_pleito(self,descricao,data_inicio,data_fim):
       pl = pleito
       pl += [{'Pleito_':""+str(len(pl)),'Descricao':descricao,'Inicio':data_inicio, 'Fim':data_fim}]
-      log.debug("Happy TurboGears Controller Responding For Duty")
+      log.debug("Salva Pleito:" + str(pl))
       return dict(
                     form=Dados().monta(pl)
                     ,next_text='Cadastra Pleito'
                     ,next_url='index'
                     ,use_case='Listagem dos Pleitos')
     @expose(template="tgvote.templates.main")
-    def cadastra_eleitor(self):
+    def adiciona_eleitor(self):
         log.debug("Cadastra Eleitor")
         return dict(
                     form=CadastroDeEleitor().monta()
--- tgvote/src/tgvote/templates/master.kid
+++ tgvote/src/tgvote/templates/master.kid
@@ -13,6 +13,29 @@
             font-family: verdana;
             text-align: right;
         }
+        #item {
+	float: left;
+	padding: 2px 2% 2px 2%;
+	margin: 2px 1% 2px 2%;
+	background: #ccc;
+	border: 1px solid #eee;
+	width: 10%; /* ie5win fudge begins */
+	voice-family: "\"}\"";
+	voice-family:inherit;
+	width: 10%;
+	}
+    #menuer {
+	float: left;
+	padding: 1px 2% 1px 2%;
+	margin: 2px 0px 2px 1%;
+	background: #aaa;
+	border: 1px solid #ccc;
+	width: 70%; /* ie5win fudge begins */
+	voice-family: "\"}\"";
+	voice-family:inherit;
+	width: 70%;
+	}
+        
     </style>
 </head>
 
@@ -29,6 +52,10 @@
     </div>
 
     <div py:if="value_of('tg_flash',False)" class="flash" py:content="tg_flash"></div>
+    <div id="menuer"> 
+      <div py:for="link,menu in tg.main_menu()"> <a href='${link}' id="item" py:content="menu"/></div>
+    </div>
+    <br/>
 
     <div py:replace="[item.text]+item[:]"/>
 
