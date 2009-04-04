d936ab293f6f1ad6ccb4c5552f17ab9b1c568d3c
cetoli <cetoli@654e2ed1-901c-0410-bc1b-db0c8d10954b>
2009-04-04 14:35:23 +0000

    artwork for abrapa

---
+++ trunk/artwork/abrapa/tuia-cconto/Neuroinformática.mm
@@ -0,0 +1,91 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1209824439464" ID="Freemind_Link_127352702" MODIFIED="1209825535364" TEXT="Neuroinform&#xe1;tica">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1209824541083" ID="Freemind_Link_1138546405" MODIFIED="1209825535141" POSITION="right" STYLE="bubble" TEXT="Produtos">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209824505442" ID="Freemind_Link_1210907336" MODIFIED="1209825535150" TEXT="Modelos">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824480655" ID="Freemind_Link_1761864431" MODIFIED="1209825535157" TEXT="Plataformas">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824466976" ID="_" MODIFIED="1209825535165" TEXT="Ferramentas">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209824972514" ID="Freemind_Link_1496954211" MODIFIED="1209825535172" POSITION="right" STYLE="bubble" TEXT="Propostas">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209824981568" ID="Freemind_Link_724554709" MODIFIED="1209825535178" TEXT="Agregar pesquisadores">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825006085" ID="Freemind_Link_1328809419" MODIFIED="1209825535186" TEXT="Acumular Informa&#xe7;&#xf5;es">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825050467" ID="Freemind_Link_1657204463" MODIFIED="1209825535195" TEXT="Engenharia Reversa">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825061125" ID="Freemind_Link_1079067846" MODIFIED="1209825535206" TEXT="Engenharia Educacional">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825106861" ID="Freemind_Link_408806967" MODIFIED="1209825535212" TEXT="Mapeamento mental">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209825132514" ID="Freemind_Link_1912890468" MODIFIED="1209825535219" POSITION="right" STYLE="bubble" TEXT="Considera&#xe7;&#xf5;es">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209825190451" ID="Freemind_Link_442812533" MODIFIED="1209825535227" TEXT="Paradigma Colaborativo">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825227018" ID="Freemind_Link_704040685" MODIFIED="1209825535234" TEXT="Instrumenta&#xe7;&#xe3;o Cient&#xed;fica">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825240116" ID="Freemind_Link_1730238217" MODIFIED="1209825535243" TEXT="Proposta Educacional">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209824574469" ID="Freemind_Link_1543352352" MODIFIED="1209825535264" POSITION="left" STYLE="bubble" TEXT="Motiva&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209824605722" ID="Freemind_Link_1437915533" MODIFIED="1209825535273" TEXT="Complexidade Mental">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824620575" ID="Freemind_Link_1639751223" MODIFIED="1209825535281" TEXT="Avan&#xe7;os Tecnol&#xf3;gicos">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824639679" ID="Freemind_Link_157457096" MODIFIED="1209825535289" TEXT="Campos Comuns">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209824686781" ID="Freemind_Link_832723945" MODIFIED="1209825535295" POSITION="left" STYLE="bubble" TEXT="Objetivos">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209824694055" ID="Freemind_Link_1845060964" MODIFIED="1209825535304" TEXT="Modelagem Mental">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824733000" ID="Freemind_Link_354359575" MODIFIED="1209825535309" TEXT="An&#xe1;lise de Dados">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824781267" ID="Freemind_Link_1327379634" MODIFIED="1209825535317" TEXT="Interven&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824802399" ID="Freemind_Link_225294877" MODIFIED="1209825535323" TEXT="Extens&#xf5;es Cibern&#xe9;ticas">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209824843194" ID="Freemind_Link_1724412364" MODIFIED="1209825535332" POSITION="left" STYLE="bubble" TEXT="Justificativa">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209824855377" ID="Freemind_Link_26917497" MODIFIED="1209825535341" TEXT="Avan&#xe7;os Te&#xf3;ricos">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209825348490" ID="Freemind_Link_255028057" MODIFIED="1209825535347" TEXT="Plasticidade Cerebral">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824882331" ID="Freemind_Link_1341789050" MODIFIED="1209825535353" TEXT="Instrumenta&#xe7;&#xe3;o Moderna">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1209824901846" ID="Freemind_Link_754952024" MODIFIED="1209825535360" TEXT="Analogia Computacional">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
